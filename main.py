6708a5bb628f623ecf084b6ce4ad88e0d2fbe624
Wornairz <wornairz@localhost.localdomain>
2018-12-31 18:53:15 +0100

    adesso il bot manda messaggi nel gruppo della redazione\n è stato implementato il markup della custom keyboard per le eventuali azioni che vorranno prendere i membri della redazione

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 # coding: utf-8
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 import datetime
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+token = open('token.conf', 'r').read()
+chatid_redazione = open('chatid.conf', 'r').read()
+
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    #print(update.message.chat_id)
 
 def domanda(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Inserisci la tua domanda")
@@ -17,15 +22,26 @@ def domanda(bot, update):
 def nuovaDomanda(bot, update):
     print(update.message.text)
     bot.sendMessage(chat_id=update.message.chat_id, text="La tua domanda è stata registrata.\nSe vuoi mandare un'altra domanda digita nuovamente \\domanda")
+    
+    #controllare se l'utente ha un username
+    keyboard =  [[InlineKeyboardButton("Approva", callback_data='1')],
+                 [InlineKeyboardButton("Disapprova", callback_data='2'),
+                  InlineKeyboardButton("Banna Utente", callback_data='3')
+                 ]
+                ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.sendMessage(chat_id=chatid_redazione, text="Nuova domanda da @" + str(update.message.from_user.username) + "\n" + update.message.text, reply_markup=reply_markup)
     return ConversationHandler.END
 
 def cancel(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Comando annullato")
     return ConversationHandler.END
 
+def button(bot, update):
+    print(str(update.callback_query))
+
 def main():
     print("Bot started at " + str(datetime.datetime.now().time()))
-    token = open('token.conf', 'r').read()
     #chatid_redazione = open('chatid.conf', 'r').read()
     
     updater = Updater(token=token)
@@ -47,8 +63,10 @@ def main():
 
         fallbacks=[cancelHandler]
     )
-
     dp.add_handler(conv_handler)
+    
+    dp.add_handler(CallbackQueryHandler(button))
+
 
     updater.start_polling()
     updater.idle()
